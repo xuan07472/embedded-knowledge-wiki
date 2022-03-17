da2a14ce966609216d5c909ab00145e4c939223f
zhangjing <zhangjing1943@163.com>
2022-03-17 23:06:52 +0800

    [ADD] 完善队列单元测试用例

--- source/lib/group_buf_queue/group_buf_queue_unitest.c
+++ source/lib/group_buf_queue/group_buf_queue_unitest.c
@@ -18,14 +18,15 @@
 
 #define GROUP_QUEUE_UNITEST
 #define ALL_UNITEST
+#define BUF_TEST_DATA ":) my first buffer message ^_^"
 
 /**
  * \brief 缓存测试
  */
 int buffer_unitest(void)
 {
-#define BUF_TEST_DATA ":) my first buffer message ^_^"
-
+    printf("```````` %s start ````````\n", __func__);
+    
     /* 1. 缓存初始化 */
     group_buf_queue_init();
     
@@ -58,36 +59,137 @@ int buffer_unitest(void)
     
     /* 7.比较测试结果 */
     int result = strcmp(buf->addr, buf_got->addr);
-    printf("--------buffer_unitest test %s--------\n\n", result ? "FAIL!": "pass.");
+    printf("________ %s test %s ________\n\n", __func__, result ? "FAIL!": "pass.");
     
     return result;
 }
 
 int pair_list_unitest(void)
 {
+    typedef struct _user_struct {
+        pair_list_node_t mynode;
+        unsigned char mydata[32];
+    } user_struct;
+
+    pair_list_t my_pair_list;
+    user_struct mystruct[16] = {0};
+    user_struct *pmystruct, *getstruct;
+    pair_list_node_t *pnode;
+
+    printf("```````` %s start ````````\n", __func__);
+
+    /* 1.初始化 */
+    pair_list_init(&my_pair_list);
+    for (int i = 0; i < 16; i++) {
+        pnode = &mystruct[i].mynode;
+        INIT_LIST_HEAD(&pnode->node);
+        pnode->pair_list = &my_pair_list;
+        pair_list_free(pnode);
+    }
     
+    /* 2.获取数据体 */
+    pnode = pair_list_alloc(&my_pair_list);
+    pmystruct = container_of(pnode, user_struct, mynode);
+    
+    /* 3.赋值数据 */
+    strcpy(pmystruct->mydata, BUF_TEST_DATA);
+    printf("put data: %s\n", pmystruct->mydata);
+    
+    /* 4.推送数据 */
+    pair_list_put(NULL, &pmystruct->mynode, PAIR_LIST_USED);
+    
+    /* 5.获取数据 */
+    pnode = pair_list_get(&my_pair_list, PAIR_LIST_USED);
+    getstruct = container_of(pnode, user_struct, mynode);
+    printf("got data: %s\n", getstruct->mydata);
+    
+    /* 6.比较数据 */
+    int result = strcmp(getstruct->mydata, BUF_TEST_DATA);
+    printf("________ %s test %s ________\n\n", __func__, result ? "FAIL!": "pass.");
+
+    /* 7.释放结构体 */
+    pair_list_free(&getstruct->mynode);
+    
+    return result;
 }
 
 int circular_linked_list_unitest(void)
 {
+	int count;
+    int result = 0;
     struct list_head entry, new1, new2;
+	struct list_head *head, *node, *n; // node和n都是临时变量，不用关心
+
+    printf("```````` %s start ````````\n", __func__);
 
     INIT_LIST_HEAD(&entry);
-    list_add(&new1, &entry);
-    list_add_tail(&new2, &entry);
     
+    printf("put a node\n");
+    list_add(&new1, &entry);
+    head = &entry;
+    count = 0;
+	if (!list_empty_careful(head)) { // 如果链表非空
+		list_for_each_safe(node, n, head) { // 循环宏定义语句
+			count++;
+		}
+	}
+    printf("node count: %d\n", count);
+    if (count != 1)
+        result = -1;
+
+    printf("put a tail node\n");
+    list_add_tail(&new2, &entry);
+    head = &entry;
+    count = 0;
+	if (!list_empty_careful(head)) { // 如果链表非空
+		list_for_each_safe(node, n, head) { // 循环宏定义语句
+			count++;
+		}
+	}
+    printf("node count: %d\n", count);
+    if (count != 2)
+        result = -1;
+
+    printf("delete a node\n");
+    list_del(&new1);
+    head = &entry;
+    count = 0;
+	if (!list_empty_careful(head)) { // 如果链表非空
+		list_for_each_safe(node, n, head) { // 循环宏定义语句
+			count++;
+		}
+	}
+    printf("node count: %d\n", count);
+    if (count != 1)
+        result = -1;
+
+    printf("________ %s test %s ________\n\n", __func__, result ? "FAIL!": "pass.");
+
     return 0;
 }
 
 #ifdef ALL_UNITEST
 int main()
 {
-    int ret = 0;
+    int ret;
+    int result = 0;
     
     ret = buffer_unitest();
+    if (ret)
+        result = -1;
+    
+    ret = pair_list_unitest();
+    if (ret)
+        result = -1;
 
-    if (0 == ret)
-        printf("%s test pass\n", __FILE__);
+    ret = circular_linked_list_unitest();
+    if (ret)
+        result = -1;
+    
+    if (result)
+        printf("%s test FAIL!\n", __FILE__);
+    else
+        printf("%s test pass.\n", __FILE__);
 
     return 0;
 }
